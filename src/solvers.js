ebaa6904287b9a5389a1bb610dd4e1d190d161e1
ojolao <jolaosho@Omotayos-MacBook-Pro.local>
2017-02-17 08:21:44 -0800

    start findNRooksSolution

--- src/solvers.js
+++ src/solvers.js
@@ -16,8 +16,8 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
-
+  var solution = new Board ({n: 5}); //fixme
+  console.log(solution);
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
